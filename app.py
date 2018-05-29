8efda9955d430adcbd1c975595fc9c431cd800ff
Pine Mizune <pinemz@gmail.com>
2018-05-30 00:37:04 +0900

    feat(app.py): add other routes

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-from aiohttp.web import Application, run_app
-from aiohttp.web_exceptions import HTTPMovedPermanently
+from aiohttp.web import Application, Response, run_app
+from aiohttp.web_exceptions import HTTPMovedPermanently, HTTPNotFound
+
+
+security_headers = {
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'DENY',
+    'X-Content-Type-Options': 'nosniff',
+}
+
+async def healthcheck(request):
+    return Response(
+        body='OK',
+        headers={
+            'Cache-Control': 'private, no-store, no-cache, must-revalidate',
+            **security_headers,
+        },
+        content_type='text/plain',
+        charset='utf-8'
+    )
 
 
 async def redirect(request):
@@ -10,14 +28,20 @@ async def redirect(request):
         'https://emoji-gen.ninja/',
         headers={
             'Cache-Control': 'public, max-age=600', # 10 min
-            'X-XSS-Protection': '1; mode=block',
-            'X-Frame-Options': 'DENY',
-            'X-Content-Type-Options': 'nosniff',
+            **security_headers,
         },
     )
 
 
+async def not_found(request):
+    return HTTPNotFound()
+
+
 app = Application()
+app.router.add_get('/healthcheck', healthcheck)
+app.router.add_get('/favicon.ico', not_found)
+app.router.add_get('/robots.txt', not_found)
+app.router.add_get('/sitemap.xml', not_found)
 app.router.add_get('/{path:.*}', redirect)
 
 
