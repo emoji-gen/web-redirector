5e03f7806957a3c55eaf2e5a36e5337ae2d8bca0
Pine Mizune <pinemz@gmail.com>
2019-03-21 21:02:15 +0900

    fix: update dependencies

--- tests/test_init.py
+++ tests/test_init.py
@@ -3,50 +3,44 @@ import pytest
 from emoji_redirect import provide_app
 
 
-async def test_healthcheck(test_client):
+@pytest.fixture
+def cli(loop, aiohttp_client):
     app = provide_app()
-    client = await test_client(app)
-    resp = await client.get('/healthcheck')
+    return loop.run_until_complete(aiohttp_client(app))
+
+
+async def test_healthcheck(cli):
+    resp = await cli.get('/healthcheck')
     assert resp.status == 200
     assert resp.headers['Content-Type'] == 'text/plain; charset=utf-8'
     assert resp.headers['Cache-Control'] == 'private, no-store, no-cache, must-revalidate'
     assert await resp.text() == 'OK'
 
 
-async def test_favicon(test_client):
-    app = provide_app()
-    client = await test_client(app)
-    resp = await client.get('/favicon.ico')
+async def test_favicon(cli):
+    resp = await cli.get('/favicon.ico')
     assert resp.status == 404
 
 
-async def test_robots(test_client):
-    app = provide_app()
-    client = await test_client(app)
-    resp = await client.get('/robots.txt')
+async def test_robots(cli):
+    resp = await cli.get('/robots.txt')
     assert resp.status == 404
 
 
-async def test_sitemap(test_client):
-    app = provide_app()
-    client = await test_client(app)
-    resp = await client.get('/sitemap.xml')
+async def test_sitemap(cli):
+    resp = await cli.get('/sitemap.xml')
     assert resp.status == 404
 
 
-async def test_redirect_root(test_client):
-    app = provide_app()
-    client = await test_client(app)
-    resp = await client.get('/', allow_redirects=False)
+async def test_redirect_root(cli):
+    resp = await cli.get('/', allow_redirects=False)
     assert resp.status == 301
     assert resp.headers['Location'] == 'https://emoji-gen.ninja/'
     assert resp.headers['Cache-Control'] == 'public, max-age=600'
 
 
-async def test_redirect_path(test_client):
-    app = provide_app()
-    client = await test_client(app)
-    resp = await client.get('/foo/bar', allow_redirects=False)
+async def test_redirect_path(cli):
+    resp = await cli.get('/foo/bar', allow_redirects=False)
     assert resp.status == 301
     assert resp.headers['Location'] == 'https://emoji-gen.ninja/foo/bar'
     assert resp.headers['Cache-Control'] == 'public, max-age=600'
