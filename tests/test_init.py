b273631b0319c26e35df5460b78d8c17cbfebda4
Pine Mizune <pinemz@gmail.com>
2019-07-18 20:34:42 +0900

    Revert "Revert "fix: fix test""
    This reverts commit a7f08c17845a38da8d429a8202b68fa9e3cfc008.

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,11 +1,11 @@
 import pytest
 
-from emoji_redirect import provide_app
+from server import app_factory
 
 
 @pytest.fixture
 def cli(loop, aiohttp_client):
-    app = provide_app()
+    app = app_factory()
     return loop.run_until_complete(aiohttp_client(app))
 
 
@@ -13,7 +13,7 @@ async def test_healthcheck(cli):
     resp = await cli.get('/healthcheck')
     assert resp.status == 200
     assert resp.headers['Content-Type'] == 'text/plain; charset=utf-8'
-    assert resp.headers['Cache-Control'] == 'private, no-store, no-cache, must-revalidate'
+    assert resp.headers['Cache-Control'] == 'private, no-cache, no-store, must-revalidate'
     assert await resp.text() == 'OK'
 
 
@@ -36,11 +36,11 @@ async def test_redirect_root(cli):
     resp = await cli.get('/', allow_redirects=False)
     assert resp.status == 301
     assert resp.headers['Location'] == 'https://emoji-gen.ninja/'
-    assert resp.headers['Cache-Control'] == 'public, max-age=600'
+    assert resp.headers['Cache-Control'] == 'private, no-cache, no-store, must-revalidate'
 
 
 async def test_redirect_path(cli):
     resp = await cli.get('/foo/bar', allow_redirects=False)
     assert resp.status == 301
     assert resp.headers['Location'] == 'https://emoji-gen.ninja/foo/bar'
-    assert resp.headers['Cache-Control'] == 'public, max-age=600'
+    assert resp.headers['Cache-Control'] == 'private, no-cache, no-store, must-revalidate'
