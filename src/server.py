c86d8f485f54b285e614241cc2d1448a929a0474
Pine Mizune <pinemz@gmail.com>
2019-07-18 20:34:30 +0900

    Revert "Revert "fix: fix endpoint name""
    This reverts commit d067072afccaf8452508194548b6f3a6ad550508.

--- src/server.py
+++ src/server.py
@@ -1,47 +1,42 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-from aiohttp.web import Application, Response
-from aiohttp.web_exceptions import HTTPMovedPermanently, HTTPNotFound
+from aiohttp.web import Application, Response, \
+    HTTPMovedPermanently, HTTPNotFound
 
+# ---------------------------------------------------------
 
-security_headers = {
+headers = {
+    'Cache-Control': 'private, no-cache, no-store, must-revalidate',
     'X-XSS-Protection': '1; mode=block',
     'X-Frame-Options': 'DENY',
     'X-Content-Type-Options': 'nosniff',
 }
 
-
-async def healthcheck(request):
+async def health(request):
     return Response(
         body='OK',
-        headers={
-            'Cache-Control': 'private, no-cache, no-store, must-revalidate',
-            **security_headers,
-        },
+        headers=headers,
         content_type='text/plain',
         charset='utf-8'
     )
-
 
 async def redirect(request):
     path = request.match_info['path']
     return HTTPMovedPermanently(
         'https://emoji-gen.ninja/{}'.format(path),
-        headers={
-            'Cache-Control': 'private, no-cache, no-store, must-revalidate',
-            **security_headers,
-        },
+        headers=headers,
     )
-
 
 async def not_found(request):
     return HTTPNotFound()
 
+# ---------------------------------------------------------
 
-def provide_app():
+def app_factory():
     app = Application()
-    app.router.add_get('/healthcheck', healthcheck)
+    app.router.add_get('/health', health)
+    app.router.add_get('/healthcheck', health)
     app.router.add_get('/favicon.ico', not_found)
     app.router.add_get('/robots.txt', not_found)
     app.router.add_get('/sitemap.xml', not_found)
