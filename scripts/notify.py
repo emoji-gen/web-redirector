7dd15ff667a12150261d534c3db8ee91b0aa5e06
Pine Mizune <pinemz@gmail.com>
2019-07-19 19:45:26 +0900

    fix: add notifications

---
+++ scripts/notify.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import slackweb
+
+username = 'CircleCI'
+icon_url = 'https://i.imgur.com/FLjAA35.png'
+author_name = 'web-redirect'
+author_icon = 'https://i.imgur.com/zPilq59l.png'
+
+
+def main():
+    message = sys.argv[1]
+    if message == 'started':
+        _notify('Deploy started', '#66d3e4')
+    elif message == 'successful':
+        _notify('Deploy successful', '#41aa58')
+    elif message == 'failed':
+        _notify('Deploy failed', '#d10c20')
+    else:
+        raise RuntimeError('Invalid message')
+
+
+def _notify(text, color):
+    if 'CIRCLE_BUILD_URL' in os.environ:
+        text += ' <{}|#{}>'.format(
+            os.environ['CIRCLE_BUILD_URL'],
+            os.environ['CIRCLE_BUILD_NUM']
+        )
+
+    slack = slackweb.Slack(url=os.environ['SLACK_INCOMING_WEBHOOK'])
+    attachment = {
+        'color': color,
+        'text': text,
+        'author_name': author_name,
+        'author_icon': author_icon,
+    }
+    slack.notify(
+        username=username,
+        icon_url=icon_url,
+        attachments=[attachment]
+    )
+
+
+if __name__ == '__main__':
+    main()
