f7af1ce3e631f79f3f83f668ce581611babbd3b4
Pine Mizune <pinemz@gmail.com>
2019-07-21 12:24:12 +0900

    fix: fix upgrade scripts

--- scripts/upgrade_requirements.sh
+++ scripts/upgrade_requirements.sh
@@ -2,6 +2,10 @@
 
 set -eu
 
+SCRIPT_DIR=`cd $(dirname ${BASH_SOURCE[0]}) && pwd`
+cd $SCRIPT_DIR/..
+
+
 if [ -n "${VIRTUAL_ENV:-}" ]; then
   printf "[ERROR] Please deactivate virtualenv before run the script.\n" 1>&2
   exit 1
